af2d6f88cab0e0ce840535c01bddc922d3f5b14f
Gustavo Gomes <gustavogomesfx@gmail.com>
2019-12-03 14:58:49 -0200

    Learning about inheritance in C#

---
+++ Inheritance1-csharp/Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Inheritance.Entities;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(905, "John doe", 200.0, 700.0);
+
+            Console.WriteLine(account.Balance);
+
+            // account.Balance = 300.00;
+        }
+    }
+}
