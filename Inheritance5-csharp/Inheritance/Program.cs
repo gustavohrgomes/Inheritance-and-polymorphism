7a8d4582e871aff1df88da92f08682421536842f
Gustavo Gomes <gustavogomesfx@gmail.com>
2019-12-04 20:35:45 -0200

    Inheritance and Polymorphism Exercise 2

---
+++ Inheritance5-csharp/Inheritance/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Inheritance.Entities;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (type == 'c' || type == 'C')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else if (type == 'u' || type == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manuactureDate = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, manuactureDate));
+                }
+                else
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsFee));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
