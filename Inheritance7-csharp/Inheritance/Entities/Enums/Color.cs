285f2d60c8f287a3646e84de3862d0050ea95622
Gustavo Gomes <gustavogomesfx@gmail.com>
2019-12-10 13:11:46 -0200

    Abstract Classes and Methods - Exercise 1

---
+++ Inheritance7-csharp/Inheritance/Entities/Enums/Color.cs
@@ -0,0 +1,9 @@
+﻿namespace Inheritance.Entities.Enums
+{
+    enum Color
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
---
+++ Inheritance7-csharp/Inheritance/Entities/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using Inheritance.Entities.Enums;
+
+namespace Inheritance.Entities
+{
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Color Color { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
